b86ec6b235fbd939a1b6d93579cf0851ce9c27a2
reimibeta <65539218+reimibeta@users.noreply.github.com>
2021-06-02 19:42:43 +0700

    .

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import setuptools
 
 setuptools.setup(
     name='product_management_models',
-    version='1.0.5',
+    version='1.0.6',
     packages=setuptools.find_packages(),
     install_requires=[
         # 'image-utils @ git+https://github.com/reimibeta/django-image-utils.git',
@@ -22,7 +22,7 @@ setuptools.setup(
         # 'drf-flex-fields==0.9.0',
         # 'django-admin-list-filter-dropdown==1.0.3',
         # 'rest-framework-utils @ git+https://github.com/reimibeta/django-rest-framework-utils.git',
-        'product-models @ git+https://github.com/reimibeta/django-product-models.git@c70cbc4eedd836a93882f00eb7489059e6598825',
+        'product-models @ git+https://github.com/reimibeta/django-product-models.git@a36b7e35037024670be725e055c3b007e2235cc9',
         # 'pillow==8.2.0',
         # 'django-cleanup==5.2.0',
         'supplier-models @ git+https://github.com/reimibeta/django-supplier-models.git@40b6bf354baeae7608bd6689585c68ef6f4f6d66',
