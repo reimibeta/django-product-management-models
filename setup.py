a5e0593578c92ac9a8884a5a016aa310ea19de94
reimibeta <65539218+reimibeta@users.noreply.github.com>
2021-06-08 20:23:00 +0700

    .

--- setup.py
+++ setup.py
@@ -14,18 +14,7 @@ setuptools.setup(
     version='1.0.7',
     packages=setuptools.find_packages(),
     install_requires=[
-        # 'image-utils @ git+https://github.com/reimibeta/django-image-utils.git',
-        # 'datetime-utils @ git+https://github.com/reimibeta/django-datetime-utils.git',
-        # 'html-render-utils @ git+https://github.com/reimibeta/django-html-render-utils.git',
-        # 'djangorestframework==3.12.4',
-        # 'djangorestframework-simplejwt==4.7.0',
-        # 'drf-flex-fields==0.9.0',
-        # 'django-admin-list-filter-dropdown==1.0.3',
-        # 'rest-framework-utils @ git+https://github.com/reimibeta/django-rest-framework-utils.git',
         'product-models @ git+https://github.com/reimibeta/django-product-models.git@a36b7e35037024670be725e055c3b007e2235cc9',
-        # 'pillow==8.2.0',
-        # 'django-cleanup==5.2.0',
         'supplier-models @ git+https://github.com/reimibeta/django-supplier-models.git@40b6bf354baeae7608bd6689585c68ef6f4f6d66',
     ]
-    # scripts=['makemigrations.py','migrate.py']
 )
