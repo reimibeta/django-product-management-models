6f92fa8654bfb985e5c86e96c13929bbc056cf7c
reimibeta <65539218+reimibeta@users.noreply.github.com>
2021-06-17 22:28:58 +0700

    .

--- product_management_models/product_supplies/class_views/product_supply_view_set.py
+++ product_management_models/product_supplies/class_views/product_supply_view_set.py
@@ -31,60 +31,45 @@ class ProductSupplyViewSet(viewsets.ModelViewSet):
 #     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)  # IsAuthenticated, IsAuthenticatedOrReadOnly
 #     authentication_classes = [JWTAuthentication, ]
 
-class ProductSupplyChartView(APIView):
-    def get(self, request):
-        # data = ProductSupply.objects.annotate(
-        #     supply_date=TruncMonth('date')
-        # ).values('supply_date').annotate(
-        #     supply_cost=Sum(
-        #         Case(
-        #             When(currency__currency='USD', then=F('cost_per_unit') / 1),
-        #             default=F('cost_per_unit') / float(4000),
-        #             output_field=DecimalField(),
-        #         )
-        #     )
-        # ).annotate(
-        #     supply_currency=Value('USD', CharField())
-        # ).order_by('supply_date')
-        # print(data)TruncMonth('date')
-        # data = ProductSupply.objects.aggregate(cost=Avg('cost_per_unit')) #stock
-        objects = ProductSupply.objects
-        # date
-        if request.method == 'GET' and 'date' in request.GET:
-            # print(DateTime.parse_date(date).month)
-            if datetime.is_date(request.GET['date']):
-                date = datetime.parse_date(request.GET['date'])
-                objects = objects.filter(date=date)
-        # month
-        if request.method == 'GET' and 'month' in request.GET:
-            # print(DateTime.parse_date(date).month)
-            if datetime.is_date(request.GET['month']):
-                # month = DateTime.parse_date(request.GET['month'])
-                objects = objects.filter(date__month=request.GET['month'])
-        # year
-        if request.method == 'GET' and 'year' in request.GET:
-            # print(DateTime.parse_date(date).month)
-            if datetime.is_date(request.GET['year']):
-                # year = DateTime.parse_date(request.GET['year'])
-                objects = objects.filter(date__year=request.GET['year'])
-        # supplier
-        if request.method == 'GET' and 'supplier' in request.GET:
-            objects = objects.filter(supplier=request.GET['supplier'])
-
-        data = objects.annotate(
-            supply_stock=F('stock__product__name')
-        ).values('supply_stock').annotate(
-            supply_qty=Sum('quantity')
-        ).annotate(
-            supply_cost=Sum(
-                Case(
-                    When(currency__currency='USD', then=(F('cost_per_unit') / 1) * F('quantity')),
-                    default=((F('cost_per_unit') / float(4000)) * F('quantity')),
-                    output_field=DecimalField(),
-                )
-            )
-        ).annotate(
-            supply_currency=Value('USD', CharField())
-        ).order_by('-stock')
-        # print(data)
-        return Response(data)
+# class ProductSupplyChartView(APIView):
+#     def get(self, request):
+#         objects = ProductSupply.objects
+#         # date
+#         if request.method == 'GET' and 'date' in request.GET:
+#             # print(DateTime.parse_date(date).month)
+#             if DateTime().is_date(request.GET['date']):
+#                 date = datetime.parse_date(request.GET['date'])
+#                 objects = objects.filter(date=date)
+#         # month
+#         if request.method == 'GET' and 'month' in request.GET:
+#             # print(DateTime.parse_date(date).month)
+#             if datetime.is_date(request.GET['month']):
+#                 # month = DateTime.parse_date(request.GET['month'])
+#                 objects = objects.filter(date__month=request.GET['month'])
+#         # year
+#         if request.method == 'GET' and 'year' in request.GET:
+#             # print(DateTime.parse_date(date).month)
+#             if datetime.is_date(request.GET['year']):
+#                 # year = DateTime.parse_date(request.GET['year'])
+#                 objects = objects.filter(date__year=request.GET['year'])
+#         # supplier
+#         if request.method == 'GET' and 'supplier' in request.GET:
+#             objects = objects.filter(supplier=request.GET['supplier'])
+#
+#         data = objects.annotate(
+#             supply_stock=F('stock__product__name')
+#         ).values('supply_stock').annotate(
+#             supply_qty=Sum('quantity')
+#         ).annotate(
+#             supply_cost=Sum(
+#                 Case(
+#                     When(currency__currency='USD', then=(F('cost_per_unit') / 1) * F('quantity')),
+#                     default=((F('cost_per_unit') / float(4000)) * F('quantity')),
+#                     output_field=DecimalField(),
+#                 )
+#             )
+#         ).annotate(
+#             supply_currency=Value('USD', CharField())
+#         ).order_by('-stock')
+#         # print(data)
+#         return Response(data)
